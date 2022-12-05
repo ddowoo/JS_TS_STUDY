c3ab2e2c5ef6205f76f55c3246654cb5ee151981
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2022-12-05 21:09:52 +0900

    그래프 생성 |  DFS,BFS 기존 코드 취소 다시하기

--- baeckjoon/그래프/1260.js
+++ baeckjoon/그래프/1260.js
@@ -1,36 +1,67 @@
 //DFS와 BFS
+// https://www.acmicpc.net/problem/1260
 
-const inputs = `4 5 1
+const inputs = `5 5 1
 1 2
-1 3
+2 3
+3 1
 1 4
-2 4
-3 4`.split("\n");
+3 5`.split("\n");
 
-const node = Number(inputs[0].split(" ")[0]);
-const line = inputs[0].split(" ")[1];
-const startNode = Number(inputs[0].split(" ")[2]);
-
+const [N, M, V] = inputs[0].split(" ").map((r) => Number(r));
 inputs.shift();
 const routes = inputs.map((input) => input.split(" ").map((string) => Number(string)));
+let graph = new Array(N + 1).fill([]);
 
-const DFS = (now, current) => {
-  if (current.length === node) {
-    return current.join(" ");
-  } else {
-    const canNext = [];
+routes.forEach((route) => {
+  const from = route[0];
+  const to = route[1];
 
-    //다음 경로로 갈곳
-    routes.forEach((route) => {
-      if (route[0] === now && !current.includes(route[1])) canNext.push(route[1]);
-      if (route[1] === now && !current.includes(route[0])) canNext.push(route[0]);
-    });
+  graph[from] = [...graph[from], to];
+  graph[to] = [...graph[to], from];
+});
+console.log("graph", graph);
+// const DFS = (now, current) => {
+//   if (current.length === N || current.length > M) {
+//     console.log(current.join(" "));
+//     return current.join(" ");
+//   } else {
+//     const canNext = [];
 
-    // 갈수 있는 곳중 가장 숫자가 작은곳
-    const next = Math.min(...canNext);
-    current.push(next);
+//     //다음 경로로 갈곳
+//     routes.forEach((route) => {
+//       if (route[0] === now && !current.includes(route[1])) canNext.push(route[1]);
+//       if (route[1] === now && !current.includes(route[0])) canNext.push(route[0]);
+//     });
+//     // 갈수 있는 곳중 가장 숫자가 작은곳
+//     console.log("canNext", canNext);
+//     const next = Math.min(...canNext);
+//     current.push(next);
 
-    DFS(next, current);
-  }
-};
-DFS(startNode, [1]);
+//     DFS(next, current);
+//   }
+// };
+
+// const BFS = () => {
+//   const stack = [V];
+
+//   for (let i = 0; i < M; i++) {
+//     const now = stack[i];
+
+//     const tempStack = [];
+//     for (let j = 0; j < M; j++) {
+//       const route = routes[j];
+//       if (route[0] === now && !stack.includes(route[1])) tempStack.push(route[1]);
+//       if (route[1] === now && !stack.includes(route[0])) tempStack.push(route[0]);
+//     }
+//     tempStack.sort((a, b) => a - b);
+//     stack.push(...tempStack);
+//     if (stack.length === N) break;
+//   }
+
+//   console.log(stack.join(" "));
+//   return stack.join(" ");
+// };
+
+// DFS(V, [V]);
+// BFS();
