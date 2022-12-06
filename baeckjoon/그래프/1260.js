a269483c00e72b10727bcba072c3121465a70523
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2022-12-06 20:40:03 +0900

    백준 1260 (런타임에러) | 테스트코드 통과

--- baeckjoon/그래프/1260.js
+++ baeckjoon/그래프/1260.js
@@ -1,18 +1,22 @@
 //DFS와 BFS
 // https://www.acmicpc.net/problem/1260
 
+// const inputs = require("fs").readFileSync(0).toString().trim().split("\n");
+//   .map((num) => second[Number(num)])
+//   .join("");
+
 const inputs = `5 5 1
 1 2
 2 3
 3 1
 1 4
 3 5`.split("\n");
+// const inputs = require("fs").readFileSync(0).toString().trim().split("\n");
 
 const [N, M, V] = inputs[0].split(" ").map((r) => Number(r));
 inputs.shift();
 const routes = inputs.map((input) => input.split(" ").map((string) => Number(string)));
 let graph = new Array(N + 1).fill([]);
-
 routes.forEach((route) => {
   const from = route[0];
   const to = route[1];
@@ -20,7 +24,58 @@ routes.forEach((route) => {
   graph[from] = [...graph[from], to];
   graph[to] = [...graph[to], from];
 });
-console.log("graph", graph);
+function MyDfs(now, answer) {
+  if (answer.length === N) return console.log(answer.join(" "));
+  const next = Math.min(...graph[now].filter((num) => !answer.includes(num)));
+
+  if (next === Infinity) {
+    for (let i = 1; i <= graph.length; i++) {
+      if (graph[i] !== [] && !answer.includes(i)) {
+        answer.push(i);
+        MyDfs(next, answer);
+        break;
+      }
+    }
+  } else {
+    answer.push(next);
+    return MyDfs(next, answer);
+  }
+}
+MyDfs(V, [V]);
+
+const MyBfs = () => {
+  const stack = [V];
+
+  for (let i = 0; i < M; i++) {
+    const now = stack[i];
+    const next = graph[now].filter((num) => !stack.includes(num));
+    next.sort((a, b) => a - b);
+    stack.push(...next);
+    if (stack.length === N) break;
+  }
+
+  return console.log(stack.join(" "));
+};
+
+MyBfs();
+
+// 참고
+// let visited = new Array(N + 1).fill(0);
+// let answer_dfs = [];
+// function DFS(v) {
+//   if (visited[v]) return;
+//   visited[v] = 1;
+//   answer_dfs.push(v);
+//   for (let i = 0; i < graph[v].length; i++) {
+//     let next = graph[v][i];
+//     if (visited[next] === 0) {
+//       DFS(next);
+//     }
+//   }
+// }
+// DFS(V);
+// console.log(answer_dfs.join(" "));
+
 // const DFS = (now, current) => {
 //   if (current.length === N || current.length > M) {
 //     console.log(current.join(" "));
@@ -40,27 +95,6 @@ console.log("graph", graph);
 
 //     DFS(next, current);
 //   }
-// };
-
-// const BFS = () => {
-//   const stack = [V];
-
-//   for (let i = 0; i < M; i++) {
-//     const now = stack[i];
-
-//     const tempStack = [];
-//     for (let j = 0; j < M; j++) {
-//       const route = routes[j];
-//       if (route[0] === now && !stack.includes(route[1])) tempStack.push(route[1]);
-//       if (route[1] === now && !stack.includes(route[0])) tempStack.push(route[0]);
-//     }
-//     tempStack.sort((a, b) => a - b);
-//     stack.push(...tempStack);
-//     if (stack.length === N) break;
-//   }
-
-//   console.log(stack.join(" "));
-//   return stack.join(" ");
 // };
 
 // DFS(V, [V]);
