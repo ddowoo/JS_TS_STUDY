c90964b0b1f7fdf0664e4e3b30cff8414e0560ef
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2023-10-07 00:34:21 +0900

    백준/ 알고리즘 기초2/브루트 포스(N과M)15652

---
+++ algorithm/baeckjoon/알고리즘 기초2/브루트 포스 (N과M)/15652_N과M.js
@@ -0,0 +1,33 @@
+const inputs = "4 2".toString().trim().split(" ");
+const fs = require("fs");
+const inputs = fs.readFileSync("./dev/stdin").toString().trim().split(" ").map(Number);
+const N = inputs[0];
+const M = inputs[1];
+
+const numArr = Array.from(new Array(N), (v, i) => i + 1);
+
+const answers = [];
+const dfs = (nowSum, leftNumArr, lastNum) => {
+  if (nowSum.length === M) {
+    answers.push(nowSum);
+  } else {
+    leftNumArr.forEach((n, i, arr) => {
+      if (n >= lastNum) {
+        const nextSum = `${nowSum}${n}`;
+        const nextLeftNumArr = arr.filter((leftNum) => leftNum >= lastNum);
+        const nextLastNum = n;
+        dfs(nextSum, nextLeftNumArr, nextLastNum);
+      }
+    });
+  }
+};
+
+numArr.forEach((num) => {
+  const nextSum = num.toString();
+  const nextLeftNumArr = numArr.filter((n) => n >= num);
+  const nextLastNum = num;
+
+  return dfs(nextSum, nextLeftNumArr, nextLastNum);
+});
+
+console.log(answers.map((v) => v.split("").join(" ")).join("\n"));
